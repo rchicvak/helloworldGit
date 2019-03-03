9e47c11fd57555e362972d6f426ef45d1fc4a645
robert chicvak <rchicvak@comcast.net>
2019-03-03 15:04:48 -0500

    Add project files.

---
+++ helloworldGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
